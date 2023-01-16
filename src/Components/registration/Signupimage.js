3676e18f8aaa1e495bd4b85174312d77040cfc07
Sharmld <sharmld@rbos.co.uk>
2023-01-16 14:07:31 +0530

    Front end spring boot hackathorn

--- src/Components/registration/Signupimage.js
+++ src/Components/registration/Signupimage.js
@@ -4,22 +4,24 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import './Registration.css'
 
 export default function ActionAreaCard() {
   return (
-    <Card sx={{ maxWidth: 700 }}>
+    <Card sx={{ maxWidth: 700 }} className="paper">
       <CardActionArea>
         <CardMedia
           component="img"
           height="500"
           image="/assets/diamond.jpg"
           alt="green iguana"
+          
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div" textAlign={'center'}>
             Natwest
           </Typography>
-          <Typography variant="body2" color="text.secondary" textAlign={'center'}>
+          <Typography className="paper" variant="body2" color="text.secondary" textAlign={'center'}>
           Our purpose is to champion the potential of people, families, and businesses.
           </Typography>
         </CardContent>
